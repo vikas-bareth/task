1545e0cf40064bdadb33409756474fe9ef841db6
Virajj <viraj28j@gmai.com>
2025-03-15 17:06:38 +0530

    add boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-3xl text-bold underline text-green-300'>
+      Hello
+    </div>
+  )
+}
+
+export default App
